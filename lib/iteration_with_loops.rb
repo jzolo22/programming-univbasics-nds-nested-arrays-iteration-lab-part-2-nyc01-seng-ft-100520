9033b1146f881591b77756adb9d32bb1e5777b6d
Julia Zolotarev <jzolotarev@gmail.com>
2020-09-12 22:33:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -16,3 +16,18 @@ def find_min_in_nested_arrays(src)
   mins_array
 end
 end
+
+row_count = 0
+even_array = []
+while row_count < src.length do
+  column_count = 0
+  while column_count < src[row_count].length do
+    if src[row_count][column_count] % 2 == 0 
+      even_array.push(src[row_count][column_count])
+    end
+    column_count += 1
+  end
+  row_count += 1
+end
+puts even_array
+end
