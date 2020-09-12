0aa93c0b12e1840adba85e2057acab379c518582
Julia Zolotarev <jzolotarev@gmail.com>
2020-09-12 22:35:18 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,27 +7,28 @@ def find_min_in_nested_arrays(src)
     while column_count < src[row_count].size do
       if src[row_count][column_count] < lowest_number
         lowest_number = src[row_count][column_count]
-        column_count += 1
-      
+  end
+  column_count += 1
   end
   mins_array << lowest_number
   row_count += 1 
-  end
-  mins_array
 end
+mins_array
 end
 
-row_count = 0
-even_array = []
-while row_count < src.length do
-  column_count = 0
-  while column_count < src[row_count].length do
-    if src[row_count][column_count] % 2 == 0 
-      even_array.push(src[row_count][column_count])
-    end
-    column_count += 1
-  end
-  row_count += 1
-end
-puts even_array
-end
+# def find_even_values(src)
+  
+# row_count = 0
+# even_array = []
+# while row_count < src.length do
+#   column_count = 0
+#   while column_count < src[row_count].length do
+#     if src[row_count][column_count] % 2 == 0 
+#       even_array.push(src[row_count][column_count])
+#     end
+#     column_count += 1
+#   end
+#   row_count += 1
+# end
+# puts even_array
+# end
